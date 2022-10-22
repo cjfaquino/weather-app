01f15038827d6a576c51db235655fadfb7978129
Christian Aquino <cjfaquino@gmail.com>
2022-10-21 20:52:06 -0700

    add 3 hour forecast

--- src/components/openWeather.js
+++ src/components/openWeather.js
@@ -2,12 +2,19 @@ import PUBLIC_WEATHER_API from "../../secret";
 
 const getWeather = async (city, state, country, units = "imperial") => {
   try {
-    const res = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${city},${state},${country}&APPID=${PUBLIC_WEATHER_API}&units=${units}`
-    );
-
-    const data = await res.json();
-    return data;
+    const [weatherRes, hourlyRes] = await Promise.all([
+      fetch(
+        `http://api.openweathermap.org/data/2.5/weather?q=${city},${state},${country}&APPID=${PUBLIC_WEATHER_API}&units=${units}`
+      ),
+      fetch(
+        `http://api.openweathermap.org/data/2.5/forecast?q=${city},${state},${country}&APPID=${PUBLIC_WEATHER_API}&units=${units}`
+      ),
+    ]);
+    const [weather, hourly] = await Promise.all([
+      weatherRes.json(),
+      hourlyRes.json(),
+    ]);
+    return { weather, hourly };
   } catch (error) {
     return console.log(error);
   }
