9bca46734becf02644e5299c2ad7e9b3b276e85c
Christian Aquino <cjfaquino@gmail.com>
2022-10-21 19:56:52 -0700

    get weather on search

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ const makeHeader = () => {
 
   header.innerHTML = `
   <header>
+    <form>
       <label for="search"
         ><input type="text" name="search" id="search"
       /></label>
-    </header>
+      <button type="submit">Search</button>
+    </form>
+  </header>
   `;
 
   return header;
@@ -34,4 +37,31 @@ const makeMain = () => {
   return main;
 };
 
+const setWeather = async (location) => {
+  const results = document.querySelector(".results");
+  const cityName = results.querySelector(".city-name");
+  const conditions = results.querySelector(".conditions");
+  const currentTemp = results.querySelector(".current-temp");
+  const currentHigh = results.querySelector("span.high-temp");
+  const currentLow = results.querySelector("span.low-temp");
+
+  const weatherData = await getWeather(location);
+
+  if (weatherData.cod === 200) {
+    cityName.textContent = weatherData.name;
+    conditions.textContent = weatherData.weather[0].main;
+    currentTemp.textContent = weatherData.main.temp;
+    currentHigh.textContent = weatherData.main.temp_max;
+    currentLow.textContent = weatherData.main.temp_min;
+  } else console.log(weatherData);
+};
+
 document.body.append(makeHeader(), makeMain());
+
+const form = document.querySelector("form");
+const search = document.getElementById("search");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  setWeather(search.value);
+});
