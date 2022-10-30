aad5f1517f514ccae6b8ca3a23738703437adb8d
Christian Aquino <cjfaquino@gmail.com>
2022-10-29 18:19:49 -0700

    install dotenv-webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   mode: "development",
@@ -22,6 +23,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new Dotenv(),
     new HtmlWebpackPlugin({
       title: "Weather App",
     }),
